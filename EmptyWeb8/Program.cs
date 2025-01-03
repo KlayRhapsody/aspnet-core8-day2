54b2b336a14f494de43be083695a4b0e8a054e68
klayhung <klayhung@gmail.com>
2025-01-03 16:07:43 +0800

    feat: 新增中介軟體以控制請求流程
    新增兩個中介軟體，分別在請求前後輸出特定字串，以便於請求流程的控制與調試。

--- EmptyWeb8/Program.cs
+++ EmptyWeb8/Program.cs
@@ -1,6 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("1");
+
+    await next();
+
+    await context.Response.WriteAsync("2");
+});
+
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("3");
+
+    await next();
+
+    await context.Response.WriteAsync("4");
+});
+
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("5");
+});
 
 app.Run();
